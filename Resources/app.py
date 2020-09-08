1f6fa26450bf2fef4e9a1008086289f78c880515
William M Davis <williammdavis@Williams-MBP.fios-router.home>
2020-09-08 17:15:54 -0400

    second

---
+++ Resources/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jasonify
+#################################################
+app = Flask(__name__)
+
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
